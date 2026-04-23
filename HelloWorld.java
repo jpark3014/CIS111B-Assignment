ea6ba8c54b7a517fe7d08d4453bc362f3a874735
jpark3014 <jpark3014@students.mc3.edu>
2026-04-23 14:09:39 -0400

    Add HelloWorld Java Program
    Created a Java program that prints loops from 1-100.

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args)  {
+        System.out.println("Hello from my CIS11B Git assignment!");
+
+
+  // Loop from 1 to 100
+  for (int i = 1; i <= 100; i++)  {
+      System.out.prinln("Number: " + i);
+  }
+
+  System.out.println("Done printing 1 through 100!");
+    }
+}
